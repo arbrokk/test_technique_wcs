aca87e80229e73f109106df8e71cfc9447eadcc2
Kayoush <brocquet.arnaud@gmail.com>
2022-01-20 10:29:23 +0100

    edit post method

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request, Depends
+from fastapi.middleware.cors import CORSMiddleware
 
 import json
 import models
@@ -8,7 +9,22 @@ from pydantic import BaseModel
 from models import Argonaute
 
 app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 models.Base.metadata.create_all(bind=engine)
+
 
 
 def get_db():
@@ -26,21 +42,20 @@ async def root():
             }
 
 
-@app.get("/get_argonautes_list")
+@app.get("/argonaute/list")
 async def get_argonautes(db: Session = Depends(get_db)):
     # query la DB pour la liste des argonautes et le return le json
     query = db.query(Argonaute).all()
     dic = {}
     i = 0
     while i < len(query):
-        dic[i] = query[i].name
+        dic[i] = {"name": query[i].name}
         i += 1
-    print(dic)
 
     return dic
 
 
-@app.get("/add_argonaute/{name}")
+@app.post("/argonaute/add")
 async def add_argonaute(name: str, db: Session = Depends(get_db)):
     # vérifie que le nom ne se trouve pas en db (prévention doublons), sinon l'ajoute en db
     if db.query(Argonaute.name).filter_by(name=name.lower()).first():
@@ -53,7 +68,7 @@ async def add_argonaute(name: str, db: Session = Depends(get_db)):
         return {"message": f"Created {argonaute.name}"}
 
 
-@app.get("/delete_argonaute/{name}")
+@app.post("/argonaute/del")
 async def delete_argonaute(name: str, db: Session = Depends(get_db)):
     # vérifie que le nom se trouve  en db si oui le supprime
     query = db.query(Argonaute).filter_by(name=name.lower())
